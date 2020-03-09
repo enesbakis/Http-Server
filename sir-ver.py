2b8966a44c917eb172bb478a222860196695c7ae
enesbakis <48660542+enesbakis@users.noreply.github.com>
2020-03-09 17:07:21 +0300

    huuppss
    the html source was retrieved from an external file

--- sir-ver.py
+++ sir-ver.py
@@ -3,15 +3,20 @@ import socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((socket.gethostname(), 80))
 server.listen(5)
+print ("{}:{} dinleniyor...".format(*server.getsockname()))
 
 while True:
     try:
         client, address = server.accept()
+        print ("{}:{} bağlandı".format(*address))
         print (client.recv(4096))
+        file = open("index.html","r",encoding= "utf-8")
         client.send(str.encode("HTTP/1.1 200 OK\n"
          +"Content-Type: text/html\n"
-         +"\n" # 
-         +"<html><body>Hello World</body></html>\n"))
+         +"Http-Server: sir-ver v0.1\n"
+         +"\n"
+         +file.read()
+         +"\n"))
          
         client.close()
     except KeyboardInterrupt:
